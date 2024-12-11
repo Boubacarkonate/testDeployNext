98daf9f4685478046844f70b86eb877594eda737
Bouba <boubacar.konate@outlook.fr>
2024-12-11 18:01:12 +0100

    route [id] change en js 6

--- src/app/api/tasks/[id]/route.tsx
+++ src/app/api/tasks/[id]/route.tsx
@@ -4,24 +4,27 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 // GET (pour récupérer une tâche)
-export function GET(req: Request, context: { params: { id: string } }) {
-  const taskId = parseInt(context.params.id, 10);
-  if (isNaN(taskId)) {
-    return NextResponse.json({ error: "ID invalide" }, { status: 400 });
-  }
+export async function GET(req: Request, context: { params: { id: string } }) {
+  const { id } = context.params; // Récupère l'ID depuis les params de l'URL
+  try {
+    const taskId = parseInt(id, 10); // Transforme l'ID en nombre
+    if (isNaN(taskId)) {
+      return NextResponse.json({ error: "ID invalide" }, { status: 400 });
+    }
 
-  return prisma.task.findUnique({
-    where: { id: taskId },
-  })
-    .then((task) => {
-      if (!task) {
-        return NextResponse.json({ error: "Tâche non trouvée" }, { status: 404 });
-      }
-      return NextResponse.json(task, { status: 200 });
-    })
-    .catch((error) => {
-      return NextResponse.json({ error: "Erreur lors de la récupération de la tâche", details: error.message }, { status: 500 });
+    const task = await prisma.task.findUnique({
+      where: { id: taskId },
     });
+
+    if (!task) {
+      return NextResponse.json({ error: "Tâche non trouvée" }, { status: 404 });
+    }
+
+    return NextResponse.json(task, { status: 200 });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return NextResponse.json({ error: "Erreur lors de la récupération de la tâche", details: error.message }, { status: 500 });
+  }
 }
 
 // PUT (pour mettre à jour une tâche)
