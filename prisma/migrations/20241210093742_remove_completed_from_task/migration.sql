8f44d5044ade6469883ee8136c366f284c847d0d
Bouba <boubacar.konate@outlook.fr>
2024-12-12 09:56:59 +0100

    update migration + npm

--- prisma/migrations/20241210093742_remove_completed_from_task/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `completed` on the `task` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `task` DROP COLUMN `completed`;
