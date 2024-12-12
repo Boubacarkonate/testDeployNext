141775706040c398187b3b92fd6b3e84707bc4b8
Bouba <boubacar.konate@outlook.fr>
2024-12-12 14:43:00 +0100

    update url1

--- src/app/services/taskService.ts
+++ src/app/services/taskService.ts
@@ -1,5 +1,5 @@
 export const fetchTasks = async () => {
-    const response = await fetch("https://test-deploy-next-u6zk.vercel.app/");
+    const response = await fetch("https://test-deploy-next-u6zk.vercel.app/api/tasks");
     if (!response.ok) {
       throw new Error("Erreur lors de la récupération des tâches");
     }
